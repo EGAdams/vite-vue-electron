7a133a947ca8599a847c62b718605bf695d09b8c
egadams <eg1972@gmail.com>
2022-03-23 12:38:04 -0400

    cleaned up

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -4,10 +4,7 @@
   <button type="button" @click="count++">
     this still works! the exact count is: {{ count }}
   </button>
-  <p>
-    Edit
-    <code>components/HelloWorld.vue</code> to test hot module replacement.
-  </p>
+
 </template>
 
 <script lang="ts">
