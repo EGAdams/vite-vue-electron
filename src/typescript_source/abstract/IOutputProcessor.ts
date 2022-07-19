4e978876c0d72975156d9d0e02b7e8a81705f225
egadams <31349790+EGAdams@users.noreply.github.com>
2022-07-19 08:51:08 -0400

    correct processOutput spelling

--- src/typescript_source/abstract/IOutputProcessor.ts
+++ src/typescript_source/abstract/IOutputProcessor.ts
@@ -5,7 +5,7 @@ import IRegex from "./IRegex";
  *  interface IOutputProcessor
  */
 interface IOutputProcessor {
-    processOuput ( commandObjectArg: ICommandObject, regexArg: IRegex ): void;
+    processOutput ( commandObjectArg: ICommandObject, regexArg: IRegex ): void;
 }
 
 export default IOutputProcessor;
