f74f9e014fbe0454ea8b8bb762f24df7c2ad362c
egadams <31349790+EGAdams@users.noreply.github.com>
2022-07-20 21:54:14 -0400

    test.js works for most queries now.

--- src/typescript_source/concrete/data/data_object/test.js
+++ src/typescript_source/concrete/data/data_object/test.js
@@ -5,7 +5,40 @@ const DataObject = require( '../../../../../out/typescript_source/concrete/data/
 const BasicConfig = require( '../../../../../out/typescript_source/config/BasicConfig.js' );
 let dataObject = new DataObject.default( new BasicConfig.default());
 
+function check_select_results( these_results ) {
+    console.log( "results inside then: " + these_results );
+    if ( these_results.length === 0 ) {
+        console.log( "test object does not exist." );
+        console.log( "inserting test object..." );
+            let object_view_id = "test_id_101";
+            let insertQuery = "insert into monitored_objects ( object_view_id, object_data ) values ( '"
+                               + object_view_id + "', '" + object_string + "' )";
+            dataObject.runSyncQuery( insertQuery ).then( function() {
+                console.log( "test object inserted." );
+                console.log( "selecting test object..." );
+                let selectObjectQuery = "select * from monitored_objects where object_view_id='" + object_view_id + "'";
+                dataObject.runSyncQuery( selectObjectQuery ).then( check_select_results );
+            });
+    } else {
+        console.log( "test object exists." );
+        //dataObject.deleteObject( id, checkExistence );
+        //let deleteQuery = "delete from monitored_objects where object_view_id='" + id + "'";
+        //dataObject.runSyncQuery( deleteQuery ).then( check_delete_results );
+    }
+}
 
+function check_delete_results() {
+    dataObject.runSyncQuery( selectObjectQuery ).then( function( delete_results ) {
+        console.log( "delete_results: " + delete_results );
+        if ( delete_results.length === 0 ) {
+            console.log( "test object deleted." );
+            
+        } else {
+            console.log( "test object not deleted." );
+            throw( Error( "*** ERROR: Can not delete test object. ***" ));
+        }
+    });
+}
 // function checkExistence( result ) {
 //     if ( result.length === 0 ) {
 //         console.log( "test object does not exist." );
@@ -43,32 +76,8 @@ console.log( "object_string: " + object_string );
 let selectObjectQuery = "select object_data from monitored_objects where object_view_id='" + id + "'";
 console.log( "selectObjectQuery: " + selectObjectQuery );
 console.log( "running query for object_data..." );
-let results = dataObject.runSyncQuery( selectObjectQuery ).then( function( these_results ) {
-    console.log( "results inside then: " + these_results );
-    if ( these_results.length === 0 ) {
-        console.log( "test object does not exist." );
-        console.log( "inserting test object..." );
-        //dataObject.insertObject( id, test_object );
-    } else {
-        console.log( "test object exists.  deleting..." );
-        //dataObject.deleteObject( id, checkExistence );
-        let deleteQuery = "delete from monitored_objects where object_view_id='" + id + "'";
-        dataObject.runSyncQuery( deleteQuery ).then( function() {
-            dataObject.runSyncQuery( selectObjectQuery ).then( function( check_delete_results ) {
-                console.log( "check_delete_results: " + check_delete_results );
-                if ( check_delete_results.length === 0 ) {
-                    console.log( "test object deleted." );
-                    console.log( "inserting test object..." );
-                    //dataObject.insertObject( id, test_object );
-                } else {
-                    console.log( "test object not deleted." );
-                    throw( Error( "*** ERROR: Can not delete test object. ***" ));
-                }
-            });
-        });
-    }
-})
-console.log( "results at end of test: " + results );
+let results = dataObject.runSyncQuery( selectObjectQuery ).then( check_select_results );
+
 
 
 // console.log( "waiting for insert..." );
